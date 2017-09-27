c3b8e37169be145c7cee5e6f5ca313ef3fad074c
lqp718 <lqp718@hotmail.com>
2017-09-27 23:47:28 +0800

    Test code

--- ShareDB.py
+++ ShareDB.py
@@ -9,19 +9,28 @@ date = datetime.datetime.strptime(cfg.StartDate, "%Y-%m-%d")
 delta = datetime.timedelta(days=1)
 i = 0
 
-# print ts.get_hist_data(cfg.ShareCode)
 while True:
 	t = random.uniform(1, 5)
-	try:
-		print ts.get_k_data(cfg.ShareCode, start=cfg.StartDate, end=cfg.StartDate)
-		df = ts.get_tick_data(cfg.ShareCode, date=date.strftime("%Y-%m-%d"), retry_count=5, pause=2, src = "tt")
-		print date.strftime("%Y-%m-%d")
-		if df != None:
-		time.sleep(t)
-		date = date + delta
-		i = i + 1
-		if i == 10:
-			i = 0
-			time.sleep(10)
-	except:
-		pass
+#	try:
+	print date.strftime("%Y-%m-%d")
+	print ts.get_k_data(cfg.ShareCode, start=date.strftime("%Y-%m-%d"), end=date.strftime("%Y-%m-%d"), autype = None)
+	df = ts.get_tick_data(cfg.ShareCode, date=date.strftime("%Y-%m-%d"), retry_count=10, pause=4)
+	# if df is not None: # src = "tt"
+	# 	print df
+	# 	break
+	if len(df) > 3: # src = "sn"
+		SortDf = df.sort_index(ascending=False,inplace=False)#.sort_values(by = 'time', axis = 0,ascending = True)
+		SortDf['Type1'] = SortDf['type'].astype('category')
+		# SortDf['Type1'].cat.categories=["neu", "buy", "sel"]
+		# SortDf['Type1'].cat.set_categories=["neu", "buy", "sel"]
+		print SortDf['Type1']
+		print len(SortDf['Type1'].cat.categories)
+	time.sleep(t)
+	date = date + delta
+	i = i + 1
+	if i == 10:
+		i = 0
+		time.sleep(10)
+#	except:
+	time.sleep(t)
+	pass
