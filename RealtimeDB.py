82e7e217d62d7536af9742e002c5524c859d0e97
liuqp1 <liuqp1@lenovo.com>
2017-10-24 18:07:20 +0800

    Test code`

--- RealtimeDB.py
+++ RealtimeDB.py
@@ -1,6 +1,7 @@
 import tushare as ts
 import time
 from pandas import DataFrame as DF
+import config as cfg
 
 per_price = None
 per_volume = None
@@ -15,15 +16,15 @@ class Realtime():
 		pass
 
 	def get(self):
-		while True:
-			df = ts.get_realtime_quotes("600050")
-
-			if self.ShareDf.empty:
+		df = ts.get_realtime_quotes(cfg.ShareCode)
+		if self.ShareDf.empty:
+			self.ShareDf = self.ShareDf.append(df, ignore_index = True)
+		else:
+			if not self.ShareDf.tail(1).equals(df):
 				self.ShareDf = self.ShareDf.append(df, ignore_index = True)
-			else:
-				# print str(getattr(df["time"], "values")[0])
-				# print str(getattr(ShareDf.iloc[-1:]["time"], "values")[0])
-				if str(getattr(df["time"], "values")[0]) != str(getattr(self.ShareDf.iloc[-1:]["time"], "values")[0]):
-					self.ShareDf = self.ShareDf.append(df, ignore_index = True)
-			time.sleep(1)
+		time.sleep(1)
 
+if __name__ == '__main__':
+	RT = Realtime()
+	while True:
+		RT.get()
