78b2c5ceeba993321e19a32c2420a7cf47289002
liuqp1 <liuqp1@lenovo.com>
2017-11-02 15:06:51 +0800

    Test code

--- RealtimeDB.py
+++ RealtimeDB.py
@@ -1,8 +1,13 @@
 import tushare as ts
 import time
-from pandas import DataFrame as df
 import config as cfg
 import json
+import datetime
+
+from pandas import DataFrame as df
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from error import trace_log
 
 per_price = None
 per_volume = None
@@ -14,6 +19,13 @@ class Realtime():
 		self.ShareInfo = df()
 		self.high = 0
 		self.low = 0
+		self.ShareDic = {
+			"_id": None,
+			"date": "XXXX-XX-XX",
+			"high": None,
+			"low": None,
+			"detail": None
+		}
 		pass
 
 	def __del__(self):
@@ -21,22 +33,23 @@ class Realtime():
 
 	def get(self):
 		df = ts.get_realtime_quotes(cfg.ShareCode)
-
 		if df is not None:
-
 			if self.ShareInfo.empty:
 				self.ShareInfo = df.loc[:, ['date', 'open', 'pre_close']]
 
-			if self.high == 0 or self.high <= df['high'].values[0]:
-				self.high = df['high'].values[0]
-
-			if self.low == 0 or self >= df['low'].values[0]:
-				self.low = df['low'].values[0]
+			if self.high == 0 or self.high <= df['high'].astype('float').values[0]:
+				self.high = df['high'].astype('float').values[0]
+			if self.low == 0 or self.low >= df['low'].astype('float').values[0]:
+				self.low = df['low'].astype('float').values[0]
 
 			vls = [cls for cls in df.columns if '_v' in cls]
 			pls = [cls for cls in df.columns if '_p' in cls]
 
 			df = df.loc[:,['time'] + ['price'] + vls + pls]
+			for col in df.columns:
+				if col == 'time':
+					continue
+				df[col] = df[col].astype('float')
 
 			if self.ShareDetail.empty:
 				self.ShareDetail = self.ShareDetail.append(df, ignore_index = True)
@@ -45,11 +58,33 @@ class Realtime():
 				if not tail.equals(df):
 					self.ShareDetail = self.ShareDetail.append(df, ignore_index = True)
 
+	def StoreToDB(self):
+		conn = MongoClient()
+		db = conn.MyShare
+		Collection = db.get_collection(name = cfg.ShareCode + "_RT")
+		if Collection is None:
+			Collection = db.create_collection(name = cfg.ShareCode + "_RT")
+		pass
+
+		self.ShareDic['_id'] = ObjectId()
+		self.ShareDic['high'] = self.high
+		self.ShareDic['low'] = self.low
+		self.ShareDic['date'] = datetime.datetime.now().strftime('%Y-%m-%d')
+
+		self.ShareDic['detail'] = json.loads(self.ShareDetail.to_json(orient = "index"))
+
+		Collection.insert_one(self.ShareDic)
+
 #Test code>>>
 if __name__ == '__main__':
 	RT = Realtime()
+	i = 0
 	while True:
 		RT.get()
 		time.sleep(1)
 		print RT.ShareDetail
+		if datetime.datetime.now().strftime('%H:%M:%S') == "15:01:00":
+			break
+
+	RT.StoreToDB()
 #Test code<<<
